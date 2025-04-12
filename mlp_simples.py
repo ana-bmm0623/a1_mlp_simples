6bfbbab82b3aefb450b840d7ed11bf1a4d10e9b8
ana-bmm0623 <anamarques@unitins.br>
2025-04-12 10:04:53 -0300

    rede neural com os hiperparâmetros iniciais

--- mlp_simples.py
+++ mlp_simples.py
@@ -115,7 +115,7 @@ if __name__ == "__main__":
     ])
     
     # Cria a rede neural com uma camada oculta de 2 neurônios
-    nn = NeuralNetwork(input_size=2, hidden_sizes=[3], output_size=1, learning_rate=0.02, epochs=5000)
+    nn = NeuralNetwork(input_size=2, hidden_sizes=[2], output_size=1, learning_rate=0.1, epochs=1000)
     
     # Treina a rede neural
     nn.train(X, y)
